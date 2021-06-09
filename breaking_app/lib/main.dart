4404d3bfeaf878ef10e26b206cc8f54a5b21bc58
Biocrysis <57637009+Biocrysis@users.noreply.github.com>
2021-06-08 22:36:13 -0700

    crear clase HOMEPAGE
    si a nuestra clase home page no la extendemos a  StatelessWidget el main nos dara error

--- breaking_app/lib/main.dart
+++ breaking_app/lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:breaking_app/src/pages/home_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      initialRoute: '/',
-      routes: {'/': (BuildContext) => HomePage()},
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: '/',
+        routes: {
+          '/': (BuildContext context) => HomePage(),
+        });
   }
 }
