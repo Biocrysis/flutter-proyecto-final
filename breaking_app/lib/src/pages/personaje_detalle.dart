1317ecbba6fdfa86ebbfeb9a8b63c84050d7e3e8
Biocrysis <57637009+Biocrysis@users.noreply.github.com>
2021-06-09 03:45:39 -0700

    crear metodos CUSTOM
    se crearon los metodos custom loading y builder
    para poder mostrar , insertar datos y tambien para ver si se estan cargando

--- breaking_app/lib/src/pages/personaje_detalle.dart
+++ breaking_app/lib/src/pages/personaje_detalle.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:breaking_app/src/models/referencia_json_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,11 +15,16 @@ class _PersonajeDetalleState extends State<PersonajeDetalle> {
   http.Client? cliente;
   static const String URLAPI = 'https://jsonplaceholder.typicode.com/posts';
 
+//clase post de referencia json model
+  List<Post>? posts;
   bool loading = false;
   bool error = false;
 
+//clase que inicial todas las variables
   @override
   void initState() {
+    // ignore: deprecated_member_use
+    posts = <Post>[];
     cliente = http.Client();
     obtenerDataFromJSON();
     // TODO: implement initState
@@ -26,7 +34,10 @@ class _PersonajeDetalleState extends State<PersonajeDetalle> {
   Future<void> obtenerDataFromJSON() async {
     http.Response respuesta = await cliente!.get(Uri.parse(URLAPI));
     if (respuesta.statusCode == 200) {
-      print(respuesta.body);
+      //creamos un mapa para decodificar el json
+      Map<String, dynamic> jsonData =
+          jsonDecode(utf8.decode(respuesta.bodyBytes));
+      print(jsonData);
     } else {
       Exception('a fallado la conexion de la respuesta');
     }
@@ -38,11 +49,32 @@ class _PersonajeDetalleState extends State<PersonajeDetalle> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      body: Container(
-        child: Text('trabajando con la api de internet'),
+        //loading? == SE ESTA CARGANDO EL CONTENIDO DE CUSTOMLOADING?
+        body:
+            Container(child: loading ? customLoading() : customListBuilder()));
+  }
+
+  Widget customLoading() {
+    return Container(
+      child: Center(
+        child: CircularProgressIndicator(),
       ),
     );
+  }
+
+  Widget customListBuilder() {
+    return SafeArea(
+        child: Container(
+      child: ListView.builder(
+        itemCount: posts!.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text('${posts![index].getTitle}'),
+            subtitle: Text('${posts![index].getBody}.'),
+          );
+        },
+      ),
+    ));
   }
 }
