248c959da03c444decc9d48cc77620ffaf710bb7
Biocrysis <57637009+Biocrysis@users.noreply.github.com>
2021-06-09 02:01:53 -0700

    importar libre HTTP
    se modifica en el pubspec.yaml
    abajo de cupertino.icons

--- breaking_app/lib/src/pages/personaje_detalle.dart
+++ breaking_app/lib/src/pages/personaje_detalle.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class PersonajeDetalle extends StatelessWidget {
+class PersonajeDetalle extends StatefulWidget {
+  PersonajeDetalle({Key? key}) : super(key: key);
+
+  @override
+  _PersonajeDetalleState createState() => _PersonajeDetalleState();
+}
+
+class _PersonajeDetalleState extends State<PersonajeDetalle> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      child: child,
+    );
   }
 }
