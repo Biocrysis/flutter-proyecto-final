98d622beb22bab3989cf2040e16f71d5a0d7d387
Biocrysis <57637009+Biocrysis@users.noreply.github.com>
2021-06-08 23:21:56 -0700

    Menu Despegable
    Creamos el menu despegable con la ayuda d ela clase DRAWER

--- breaking_app/lib/src/pages/home_page.dart
+++ breaking_app/lib/src/pages/home_page.dart
@@ -4,7 +4,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Breaking Bad Personajes'),
+        title: Text('Menu'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text('Personajes'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text("Episiodos"),
+              onTap: () {},
+            )
+          ],
+        ),
       ),
       body: Container(
         child: Column(
