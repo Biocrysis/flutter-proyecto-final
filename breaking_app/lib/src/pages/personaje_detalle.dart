ecde4f7fbcae52f2a0c74a80ac38b27ad12df606
Biocrysis <57637009+Biocrysis@users.noreply.github.com>
2021-06-09 03:01:25 -0700

    obtuvimos respuesta de la API en internet
    como no nos dejaba poner
     http.Response respuesta = await cliente!.get(URLAPI);
    porque String no igual del tipo Uri
    tuvimos que parsearlo
     http.Response respuesta = await cliente!.get(Uri.parse(URLAPI));

--- breaking_app/lib/src/pages/personaje_detalle.dart
+++ breaking_app/lib/src/pages/personaje_detalle.dart
@@ -9,10 +9,24 @@ class PersonajeDetalle extends StatefulWidget {
 }
 
 class _PersonajeDetalleState extends State<PersonajeDetalle> {
+  http.Client? cliente;
+  static const String URLAPI = 'https://jsonplaceholder.typicode.com/posts';
+
   @override
   void initState() {
+    cliente = http.Client();
+    obtenerDataFromJSON();
     // TODO: implement initState
     super.initState();
+  }
+
+  Future<void> obtenerDataFromJSON() async {
+    http.Response respuesta = await cliente!.get(Uri.parse(URLAPI));
+    if (respuesta.statusCode == 200) {
+      print(respuesta.body);
+    } else {
+      Exception('a fallado la conexion de la respuesta');
+    }
   }
 
   @override
