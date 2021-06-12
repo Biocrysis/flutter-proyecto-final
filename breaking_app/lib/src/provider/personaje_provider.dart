babfce5445866675487c3d36e45cc9a3f9cf16bc
Biocrysis <57637009+Biocrysis@users.noreply.github.com>
2021-06-12 00:24:05 -0700

    Revert "Revert "agregar gif loading""
    This reverts commit 45d7737d92c17917f954c801eea7be5759f04a40.

--- breaking_app/lib/src/provider/personaje_provider.dart
+++ breaking_app/lib/src/provider/personaje_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:breaking_app/src/models/personajes_models.dart';
@@ -19,6 +20,9 @@ class _PersonajeProviderState extends State<PersonajeProvider> {
   bool loading = false;
   bool error = false;
 
+//DISEÑO PARA LA LISTA DE IMAGENS DE LOS PERSONAJES
+  List<int> _listNumero = [];
+  int ultimoItem = 0;
 //clase que inicial todas las variables
   @override
   void initState() {
@@ -48,8 +52,19 @@ class _PersonajeProviderState extends State<PersonajeProvider> {
   }
 
   @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
         //loading? == SE ESTA CARGANDO EL CONTENIDO DE CUSTOMLOADING?
         body:
             Container(child: loading ? customLoading() : customListBuilder()));
@@ -62,22 +77,62 @@ class _PersonajeProviderState extends State<PersonajeProvider> {
       ),
     );
   }
+  /* Widget _crearLista() {
+    return RefreshIndicator(
+      onRefresh: obtenerPagina1,
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: _listNumero.length,
+        itemBuilder: (BuildContext context, index) {
+          final _imagen = _listNumero[index];
+          return FadeInImage(
+            placeholder: AssetImage(''),
+            image:
+                NetworkImage('https://picsum.photos/500/500/?images=$_imagen'),
+          );
+        },
+      ),
+    );
+  } */
 
   Widget customListBuilder() {
     return SafeArea(
+        top: true,
         child: Container(
-      child: posts!.isEmpty
-          ? deataEmpty()
-          : ListView.builder(
-              itemCount: posts!.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text('${posts![index].name}'),
-                  // subtitle: Text('${posts![index].getBody}.'),
-                );
-              },
-            ),
-    ));
+          child: posts!.isEmpty
+              ? deataEmpty()
+              : ListView.builder(
+                  itemCount: posts!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return SafeArea(
+                        child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ListTile(
+                            title: Text('${posts![index].name}'),
+                            subtitle: FadeInImage(
+                                placeholder: AssetImage("assets/loadnier.gif"),
+                                image: NetworkImage('${posts![index].img}')),
+                            onTap: () {
+                              _showMyDialog(context);
+                              ultimoItem = index;
+                            },
+                          )
+                        ],
+                      ),
+                    ));
+
+                    /* return ListTile(
+                      title: Text('${posts![index].name}'),
+                      subtitle: FadeInImage(
+                        placeholder: AssetImage("assets/loadnier.gif"),
+                        image: NetworkImage('${posts![index].img}'),
+                      ),
+                    ); */
+                  },
+                ),
+        ));
   }
 
   Widget deataEmpty() {
@@ -89,6 +144,22 @@ class _PersonajeProviderState extends State<PersonajeProvider> {
           )
         ],
       ),
+    );
+  }
+
+//creamos la alerta
+  Widget _alertDialog(context) {
+    return AlertDialog(
+      title: Text('Sobre Nombre'),
+      content: Text("${posts![ultimoItem].nickname}"),
+    );
+  }
+
+  //creamos la alerta
+  Future _showMyDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (_) => _alertDialog(context),
     );
   }
 }
