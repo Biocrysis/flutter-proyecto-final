eb7df0b76c773250f66398cf0e6aefa9e986aa57
Biocrysis <57637009+Biocrysis@users.noreply.github.com>
2021-06-12 00:59:06 -0700

    arreglando el not null del SWIPER
    en el main agregamos en la primera linea // @dart=2.9
     y para el error del render del swiper agregue  flexible() al swiper

--- breaking_app/lib/src/pages/home_page.dart
+++ breaking_app/lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -32,6 +33,10 @@ class HomePage extends StatelessWidget {
           filter: ImageFilter.blur(sigmaX: 6.0, sigmaY: 6.0),
           child: Container(
             decoration: BoxDecoration(color: Colors.white.withOpacity(0.01)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [Flexible(child: _swiperTarjeta()), Text('ola')],
+            ),
           ),
         ),
       ),
@@ -39,6 +44,16 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperTarjeta() {
-    return Container();
+    return new Swiper(
+      itemBuilder: (BuildContext context, index) {
+        return new Image.network(
+          'http://via.placeholder.com/288x188',
+        );
+      },
+      itemCount: 3,
+      itemHeight: 300,
+      itemWidth: 400,
+      scale: 0.5,
+    );
   }
 }
