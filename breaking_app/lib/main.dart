f8f399d0e946c33ddf480997658438e15bd359b8
Biocrysis <57637009+Biocrysis@users.noreply.github.com>
2021-06-12 01:33:48 -0700

    Revert "arreglando el not null del SWIPER"
    This reverts commit eb7df0b76c773250f66398cf0e6aefa9e986aa57.

--- breaking_app/lib/main.dart
+++ breaking_app/lib/main.dart
@@ -1,4 +1,3 @@
-// @dart=2.9
 import 'package:breaking_app/src/provider/frases_provider.dart';
 import 'package:breaking_app/src/provider/muertos_provider.dart';
 import 'package:breaking_app/src/provider/personaje_provider.dart';
